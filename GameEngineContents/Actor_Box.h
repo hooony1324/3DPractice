2c77302a315ae601bc0d780348e8b06504676dc6
성훈 김 <hooony1324@gmail.com>
2022-10-21 22:17:16 +0900

    김성훈 : 엔진(~쓰레드풀) 커밋

---
+++ GameEngineContents/Actor_Box.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <GameEngineCore/GameEngineActor.h>
+
+// Ό³Έν :
+class Actor_Box : public GameEngineActor
+{
+public:
+	// constrcuter destructer
+	Actor_Box();
+	~Actor_Box();
+
+	// delete Function
+	Actor_Box(const Actor_Box& _Other) = delete;
+	Actor_Box(Actor_Box&& _Other) noexcept = delete;
+	Actor_Box& operator=(const Actor_Box& _Other) = delete;
+	Actor_Box& operator=(Actor_Box&& _Other) noexcept = delete;
+
+protected:
+	void Start() override;
+	void Update(float _DeltaTime) override;
+	void End() override;
+
+private:
+	GameEngineTextureRenderer* Renderer;
+	float4 ResultColor;
+};
+
---
+++ GameEngineContents/Actor_Plane.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <GameEngineCore/GameEngineActor.h>
+
+// Ό³Έν :
+class Actor_Plane : public GameEngineActor
+{
+public:
+	// constrcuter destructer
+	Actor_Plane();
+	~Actor_Plane();
+
+	// delete Function
+	Actor_Plane(const Actor_Plane& _Other) = delete;
+	Actor_Plane(Actor_Plane&& _Other) noexcept = delete;
+	Actor_Plane& operator=(const Actor_Plane& _Other) = delete;
+	Actor_Plane& operator=(Actor_Plane&& _Other) noexcept = delete;
+
+protected:
+	void Start() override;
+	void Update(float _DeltaTime) override;
+	void End() override;
+
+private:
+	GameEngineTextureRenderer* Renderer;
+	float4 ResultColor;
+};
+
